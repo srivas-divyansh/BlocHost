d477c4f38ddbe51b3ea9d4ec40238d50733812ae
Yashrajdps13 <yashrajdps@gmail.com>
2024-11-09 16:57:43 +0530

    test dedployment

--- src/components/DepositFunds.js
+++ src/components/DepositFunds.js
@@ -1,18 +1,18 @@
 const ethers = require('ethers');
 
-function DepositFunds({state}){
+function DepositFunds({ state }) {
     const deposit = async (event) => {
         event.preventDefault();
-        const {contract} = state;
+        const { contract } = state;
         const amount = document.getElementById('amount').value;
-        const transaction = await contract.depositFunds({value: ethers.parseEther(amount)});
+        const transaction = await contract.depositFunds({ value: ethers.parseEther(amount) });
         await transaction.wait();
         console.log('Funds deposited');
     }
     return (
         <div>
             <form onSubmit={deposit}>
-                <label>Amount</label>
+                <label>Amount1</label>
                 <input type="text" id="amount" name="amount" required />
                 <button type="submit">Deposit</button>
             </form>
